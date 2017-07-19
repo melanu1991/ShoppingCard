a37faf222dbbdc712dc79f260def3627f6b5f053
melanu1991 <melanu284733@gmail.com>
2017-07-19 14:09:07 +0300

    Добавлен CoreDataManager

--- ShoppingCard/ShoppingCard/VAKCoreDataManager.h
+++ ShoppingCard/ShoppingCard/VAKCoreDataManager.h
@@ -1,5 +1,17 @@
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 
 @interface VAKCoreDataManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (VAKCoreDataManager *)sharedManager;
++ (NSManagedObjectContext *)managedObjectContext;
++ (void)setupCoreDataStorageName:(NSString *)name;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
